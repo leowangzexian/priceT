16cba7e95f47fbb6414d9540acf9a22d6d846fb3
leowangzexian <statistics.leo@gmail.com>
2024-11-10 16:52:47 -0600

    added the function seasonal in seasonal.R

---
+++ R/seasonal.R
@@ -0,0 +1,7 @@
+# Function that returns the estimated coefficients of the seasonality function
+# temp = n by 1 vector containing the past temperatures data at one station over (n / 365) years
+seasonal = function(temp) {
+
+
+  return(c(a, b, c, d)) # returns the estimated coefficients of the seasonality function
+}
