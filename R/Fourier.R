39c41bb386eb64610dc1fff135928dd51adf331e
leowangzexian <statistics.leo@gmail.com>
2024-11-09 23:19:20 -0600

    specified the input parameters for the function Fourier in Fourier.R

---
+++ R/Fourier.R
@@ -0,0 +1,10 @@
+# Function that returns the price of a temperature futures using the truncated Fourier series method
+# temp = n by p matrix containing the past temperatures data at p stations over (n / 365) years
+# station = the specific station that the price of the temperature futures depends on
+# start_ind = index of the start of the measurement period
+# end_ind = index of the end of the measurement period
+# type = type of the temperature futures, CDD, HDD or CAT
+Fourier = function(temp, station, start_ind, end_ind, type) {
+
+}
+
