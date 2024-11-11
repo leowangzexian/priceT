678d3a7074d0ac51d101cd168d3866d6c4074051
leowangzexian <statistics.leo@gmail.com>
2024-11-10 20:59:20 -0600

    changed the return to a list in the function seasonal in seasonal.R

--- R/seasonal.R
+++ R/seasonal.R
@@ -7,7 +7,7 @@ seasonal = function(temp) {
   }
 
   n = length(temp)
-  t = 1:n # times indices
+  t = c(1:n) # times indices
   initial = c(50, 0, 35, 0) # initialise parameter values for optimisation
 
   # perform minimisation
@@ -25,11 +25,11 @@ seasonal = function(temp) {
   # plotting the temperatures and seasonal function
   plot(t, temp, type = "l", col = "blue", xlab = "Time", ylab = "Temperature", lwd = 2)
   lines(t, loc1fitted, col = "gold", lwd = 2.5)
-  saved_plot = recordPlot() # saving the plot
+  saved_plot = recordPlot() # save the plot
 
   # returns the estimated coefficients of the seasonality function,
   # the fitted seasonality function and the graph plotted
-  return(c(a = loc1a, b = loc1b, c = loc1c, d = loc1d, seasonality = loc1fitted, plt = saved_plot))
+  return(list(a = loc1a, b = loc1b, c = loc1c, d = loc1d, seasonality = loc1fitted, plt = saved_plot))
 }
 
 # Function that computes and returns the objective function for fitting the seasonality function
@@ -38,7 +38,7 @@ loc1temp = function(params, t, loc1temperatures) {
   S = params[1] + params[2] * t + params[3] * cos(2 * pi * (t - params[4]) / 365)
 
   # computes the squared difference between the seasonality and the observed values of the temperature
-  S2 = S^2 - 2 * S * loc1temperatures + loc1temperatures^2
+  S2 = sum(S^2 - 2 * S * loc1temperatures + loc1temperatures^2)
 
   return(S2) # returns the objective function
 }
