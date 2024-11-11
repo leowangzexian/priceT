0bbbb82d21b62e908b076399ed129197e28b05a2
leowangzexian <statistics.leo@gmail.com>
2024-11-10 23:50:16 -0600

    added compatibility checks and changed the return to a list for the function Fourier in Fourier.R

--- R/Fourier.R
+++ R/Fourier.R
@@ -5,8 +5,19 @@
 # end_ind = index of the end of the measurement period
 # type = type of the temperature futures, CDD, HDD or CAT
 Fourier = function(residuals, station, start_ind, end_ind, type) {
+  # compatibility checks
+  if (is.matrix(resid) == FALSE) {
+    stop("resid should be a vector.") # returns error message if the input resid is not a matrix
+  }
   n = dim(residuals)[1]
   p = dim(residuals)[2]
+  if (n < 365 * 2) {
+    stop("Need at least two years of data for resid.") # returns error message if the data inputted is less than 2 years
+  }
+  if (p < 3) {
+    sto("Need at least three stations.") # returns error message if the number of stations < 3
+  }
+
   years = n / 365 # number of years included
   residt = do.call(ts.union, lapply(1:p, function(i) as.ts(residuals[, i]))) # format resid as time series
   residtar = ar(residt, aic = FALSE, order.max = 3) # fit to VAR model
@@ -69,8 +80,8 @@ Fourier = function(residuals, station, start_ind, end_ind, type) {
 
   }
 
-  # returns the computed futures price
-  return(price)
+  # returns the computed futures price, standardized residuals and the graph plotted
+  return(list(price = price, sresids = resids, plt = saved_plot))
 }
 
 # Function that computes and returns the objective function for fitting the seasonal variance function based on the truncated Fourier series
