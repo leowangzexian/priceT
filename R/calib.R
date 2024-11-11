3c5dcb3ce68cbb9b522419d46aa3a86d5eb558a5
leowangzexian <statistics.leo@gmail.com>
2024-11-11 01:27:57 -0600

    started working on the function calib in calib.R, adding basic structures and documentation

---
+++ R/calib.R
@@ -0,0 +1,34 @@
+#' Calibrating risk-neutral parameters
+#'
+#' @description
+#' Function that returns the calibrated risk-neutral parameters and in-sample pricing errors based on the market prices
+#'
+#' @param theta A q times 1 vector
+#' @param price A k times 1 vector containing the computed futures prices
+#' @param market_price A k times 1 vector containing the actual futures prices traded in the market
+#'
+#' @return A list containing:
+#'         \item{}{}
+#'         \item{}{}
+#'         \item{}{}
+#'
+#' @export
+#'
+#' @examples
+calib = function(theta, price, market_price) {
+  # compatibility checks
+  if (is.vector(theta) == FALSE) {
+    stop("temp should be a vector.") # returns error message if the input temp is not a vector
+  }
+  if (is.vector(price) == FALSE) {
+    stop("temp should be a vector.") # returns error message if the input temp is not a vector
+  }
+  if (is.vector(market_price) == FALSE) {
+    stop("temp should be a vector.") # returns error message if the input temp is not a vector
+  }
+
+  # computing price with risk-neutral parameter
+
+  # returns the objective function for minimisation
+  return(theta^2 - 2 * theta * market_price - market_price^2)
+}
