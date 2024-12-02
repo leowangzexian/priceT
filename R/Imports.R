43f58ea46a87fc0b6bc3fb04b9ec384b55ab4b9f
leowangzexian <statistics.leo@gmail.com>
2024-12-02 05:21:16 -0600

    added the package expm and function expm in Imports.R

--- R/Imports.R
+++ R/Imports.R
@@ -3,8 +3,10 @@
 #' @import ghyp
 #' @import ggplot2
 #' @import stats
+#' @import expm
 #' @importFrom vars ar
 #' @importFrom dgof ks.test
 #' @importFrom Rcpp evalCpp
+#' @importFrom expm expm
 #' @useDynLib priceT, .registration = TRUE
 NULL
