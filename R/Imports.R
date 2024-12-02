88882565321fed95ab87d9abb7ae23c62dce60bc
leowangzexian <statistics.leo@gmail.com>
2024-12-02 15:43:02 -0600

    imported the package zoo and the function na.approx by updating the description file and Imports.R

--- R/Imports.R
+++ R/Imports.R
@@ -4,12 +4,14 @@
 #' @import ggplot2
 #' @import stats
 #' @import expm
+#' @import zoo
 #' @importFrom vars ar
 #' @importFrom dgof ks.test
 #' @importFrom expm expm
 #' @importFrom ghyp fit.NIGmv
 #' @importFrom ghyp qqghyp
 #' @importFrom ghyp fit.ghypuv
+#' @importFrom zoo na.approx
 #' @importFrom Rcpp evalCpp
 #' @useDynLib priceT, .registration = TRUE
 NULL
