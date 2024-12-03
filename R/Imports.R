a854c595aad633fd82fe0f0b91c412465c72d2c4
leowangzexian <statistics.leo@gmail.com>
2024-12-03 09:53:52 -0600

    updated the file Imports.R

--- R/Imports.R
+++ R/Imports.R
@@ -5,13 +5,7 @@
 #' @import stats
 #' @import expm
 #' @import zoo
-#' @importFrom vars ar
-#' @importFrom dgof ks.test
 #' @importFrom expm expm
-#' @importFrom ghyp fit.NIGmv
-#' @importFrom ghyp qqghyp
-#' @importFrom ghyp fit.ghypuv
-#' @importFrom ghyp rghyp
 #' @importFrom zoo na.approx
 #' @importFrom Rcpp evalCpp
 #' @useDynLib priceT, .registration = TRUE
