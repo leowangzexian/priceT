613e6377c7b87e67cb0a361bb1839b283f1f0c10
leowangzexian <statistics.leo@gmail.com>
2024-12-09 20:27:05 -0600

    updated the file test-diagnostics.R

--- tests/testthat/test-diagnostics.R
+++ tests/testthat/test-diagnostics.R
@@ -15,8 +15,8 @@ test_that("the function diagnostics outputs appropriate test statistics and p-va
   # 1st test case
   resid = as.numeric(ghyp::rghyp(730)) # deseasonalized temperaures at one station
   diag1 = diagnostics(resid)
-  expect_gt(diag1$teststat, 0.05) # test statistic less than 0.3
-  expect_lt(diag1$pvalue, 0.04) # p-value is 0
+  expect_gt(diag1$teststat, 0.01) # test statistic less than 0.3
+  expect_lt(diag1$pvalue, 0.01) # p-value is 0
 
   # 2nd test case
   resid = as.numeric(ghyp::rgig(730)) # deseasonalized temperaures at one station
